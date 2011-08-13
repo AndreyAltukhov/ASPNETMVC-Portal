6add2c1fb3276e175bfae1b35f307aa7ba3c9a01
Andrey Altukhov <andrey.altukhov@gmail.com>
2011-08-14 00:39:58 +0300

    Added default folder in ASNETMVC-Portal.Core project.

---
+++ ASPNETMVC-Portal.Core/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNETMVC_Portal.Core.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class IRepository<T>
+    {
+
+    }
+}
